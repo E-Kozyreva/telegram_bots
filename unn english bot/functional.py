947b96833dfd5f953cb3aba5afb1988db9c30773
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-07-15 23:12:54 +0300

    code

---
+++ unn english bot/functional.py
@@ -0,0 +1,26 @@
+from tkinter.tix import Select
+from deep_translator import GoogleTranslator
+import random
+
+# OPEN FILE
+f = open("engwords.txt", "r")
+words = f.read()
+words_list = words.split("\n")
+
+# GENERATE RANDOM WORDS
+def get_random_word():
+    random_words = random.choices(words_list, k=4)
+    word = random.choice(random_words).lower()
+    return random_words, word
+
+
+# TRANSLATE WORDS
+def translation(words, word):
+    r_answer = GoogleTranslator(source='en', target='ru').translate(word).lower()
+    w1 = GoogleTranslator(source='en', target='ru').translate(words[0]).lower()
+    w2 = GoogleTranslator(source='en', target='ru').translate(words[1]).lower()
+    w3 = GoogleTranslator(source='en', target='ru').translate(words[2]).lower()
+    w4 = GoogleTranslator(source='en', target='ru').translate(words[3]).lower()
+    translation_list = [w1, w2, w3, w4]
+    return r_answer, translation_list
+
