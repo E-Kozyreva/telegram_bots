3bb9b288cbfde2ac4a1f4f6249da4700fc7a94cc
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-10-28 18:20:39 +0300

    Update main.py

--- unn english bot/main.py
+++ unn english bot/main.py
@@ -1,19 +1,18 @@
 import telebot
 import data
-import functional
+import buttons
+import translate
 from telebot import types
 
+
 # TOKEN
-bot = telebot.TeleBot("5431463396:AAGpCBdMOJ0ZoLhY2rZFjKyvnQOUrGrOBF4");
+bot = telebot.TeleBot("5431463396:AAGpCBdMOJ0ZoLhY2rZFjKyvnQOUrGrO...");
+
 
 # START
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Поздороваться 👋")
-    btn2 = types.KeyboardButton("Обратная связь 👩‍💻")
-    markup.add(btn1, btn2)
-    msg = bot.send_message(message.from_user.id, text="Привет, {0.first_name}! Я бот для изучения английского языка! 👩‍💻".format(message.from_user), reply_markup=markup)
+    msg = bot.send_message(message.from_user.id, text="Привет, {0.first_name}! Я бот для изучения английского языка! 👩‍💻".format(message.from_user), reply_markup=buttons.get_first_message())
     bot.register_next_step_handler(msg, t_functions)
 
 
@@ -21,10 +20,7 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def t_functions(message):
     if(message.text == "Поздороваться 👋"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        start = types.KeyboardButton("Давай 👀")
-        markup.add(start)
-        msg = bot.send_message(message.from_user.id, text="Привет! Давай учить новые слова 😊", reply_markup=markup)
+        msg = bot.send_message(message.from_user.id, text="Привет! Давай учить новые слова 😊", reply_markup=buttons.hello())
         bot.register_next_step_handler(msg, t_translation)
     elif(message.text == "Обратная связь 👩‍💻"):
         bot.send_message(message.from_user.id, "По всем вопросам обращайся к @kozyreva_k1 🙃")
@@ -39,29 +35,18 @@ def t_translation(message):
         global random_words, word
         global r_answer, translation_list
 
-        random_words, word = functional.get_random_word()
-
+        random_words, word = translate.get_random_word()
         u_id = message.from_user.id
 
         data.user_data[u_id] = [random_words, word]
-        r_answer, translation_list = functional.translation(data.user_data[u_id][0], data.user_data[u_id][1])
+        r_answer, translation_list = translate.translation(data.user_data[u_id][0], data.user_data[u_id][1])
         data.user_answer[u_id] = [translation_list, r_answer]
 
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        trn1 = types.KeyboardButton(data.user_answer[u_id][0][0])
-        trn2 = types.KeyboardButton(data.user_answer[u_id][0][1])
-        trn3 = types.KeyboardButton(data.user_answer[u_id][0][2])
-        trn4 = types.KeyboardButton(data.user_answer[u_id][0][3])
-        markup.add(trn1, trn2, trn3, trn4)
-        msg = bot.send_message(message.from_user.id, text="Как переводится слово «{0}» 🧐".format(data.user_data[u_id][1]), reply_markup=markup)
+        msg = bot.send_message(message.from_user.id, text="Как переводится слово «{0}» 🧐".format(data.user_data[u_id][1]), reply_markup=buttons.user_data(message.from_user.id))
         bot.register_next_step_handler(msg, t_checking)
     elif (answer == "Стоп 🚫"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Поздороваться 👋")
-        btn2 = types.KeyboardButton("Обратная связь 👩‍💻")
-        markup.add(btn1, btn2)
-        msg = bot.send_message(message.from_user.id, text="Эх, пока 😔", reply_markup=markup)
-        bot.register_next_step_handler(msg, t_functions)
+        msg = bot.send_message(message.from_user.id, text="Эх, пока 😔", reply_markup=buttons.stop())
+        bot.register_next_step_handler(msg, start)
 
 
 # CHECK USER ANSWER
@@ -69,28 +54,14 @@ def t_checking(message):
     answer = message.text
     u_id = message.from_user.id
 
-    print(data.user_answer)
-    print()
-
     if (answer == data.user_answer[u_id][1]):
         bot.send_message(message.from_user.id, "Верно, молодец! ❤️")
-
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        repeat = types.KeyboardButton("Ещё 🙂")
-        stop = types.KeyboardButton("Стоп 🚫")
-        markup.add(repeat, stop)
-
-        msg = bot.send_message(message.from_user.id, text="Ещё? 🙃", reply_markup=markup)
+        msg = bot.send_message(message.from_user.id, text="Ещё? 🙃", reply_markup=buttons.repeat())
         bot.register_next_step_handler(msg, t_translation)
     elif (answer != data.user_answer[u_id][1]):
         bot.send_message(message.from_user.id, "Неверно, слово «{0}» переводится как «{1}» 😶‍🌫️".format(data.user_data[u_id][1], data.user_answer[u_id][1]))
 
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        repeat = types.KeyboardButton("Ещё 🙂")
-        stop = types.KeyboardButton("Стоп 🚫")
-        markup.add(repeat, stop)
-
-        msg = bot.send_message(message.from_user.id, text="Ещё? 🙃", reply_markup=markup)
+        msg = bot.send_message(message.from_user.id, text="Ещё? 🙃", reply_markup=buttons.repeat())
         bot.register_next_step_handler(msg, t_translation)
 
 
