c6bcecc20d05a10f6ac0b27164b4772d3f437974
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-11-10 18:20:06 +0300

    add function "send file"

--- shorty chat-bot/main.py
+++ shorty chat-bot/main.py
@@ -1,35 +1,34 @@
 import telebot
 from telebot import types
-import books
 import buttons
+import books
 
 
 # TOKEN
 bot = telebot.TeleBot("5634218236:AAE5ECZb4LXJE75AGEpOuSLT9GRtwrccEE8");
-url = "https://api.telegram.org/bot"
 
 
 # START
 @bot.message_handler(commands=['start'])  
-def start_command(message):  
+def t_start_command(message):  
     bot.send_message(message.chat.id,  "Привет, я чат-бот!")
 
 
-# GET BOOKS
-@bot.message_handler(commands=["get_book"])
+# GET BOOK
+@bot.message_handler(commands=["books"])
 def t_get_book(message):
-    msg = bot.send_message(message.chat.id, text="Выбери файл :)", reply_markup=buttons.get_book())
+    msg = bot.send_message(message.chat.id, text="Выбери файл, который хочешь скачать 🙃", reply_markup=buttons.get_book())
     bot.register_next_step_handler(msg, t_send_book)
 
 
+@bot.message_handler(content_types=['text'])
 def t_send_book(message):
     answer = message.text
     if answer in books.book:
-        pdf = open(f'files/{answer}.pdf', 'rb')
-        bot.send_document(message.chat.id, pdf)
-        pdf.close()
+        msg = bot.send_document(message.chat.id, document=books.book[answer], reply_markup=types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(msg, t_commands)
     else:
-        msg = bot.send_message(message.chat.id, text="Такого файла нет, попробуй ещё раз", reply_markup=buttons.get_book())
+        msg = bot.send_message(message.chat.id, text="Такого файла нет, попробуй ещё раз 😔", reply_markup=buttons.get_book())
         bot.register_next_step_handler(msg, t_send_book)
 
 
