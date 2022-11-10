7c986e23fbe9699ee7bb733b895fe6810ca34e7d
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-11-10 18:19:07 +0300

    add file_id

--- shorty chat-bot/book.py
+++
@@ -1,5 +0,0 @@
-book = ["Идивидуальное чтение", 
-        "English for mathematics", 
-        "Test your English", 
-        "Перевод англоязычных текстов", 
-        "Remedial English", ]
---
+++ shorty chat-bot/books.py
@@ -0,0 +1,5 @@
+book = {"Идивидуальное чтение": 'BQACAgIAAxkBAAMPY2z5QacGrUeYb5voqA3_UPs4ylUAAoEeAALIWmlLcU52I6V1cmIrBA', 
+        "English for mathematics": 'BQACAgIAAxkBAAMGY2z41NVTgjBOWH9tGvIBUgxw-1YAAnseAALIWmlLlKFBTHlmrbErBA', 
+        "Test your English": 'BQACAgIAAxkBAAMMY2z5A5VenBeUcXrOJV2LiJlW2aYAAn0eAALIWmlLKm4P39kOBVsrBA', 
+        "Перевод англоязычных текстов": 'BQACAgIAAxkBAAMSY2z5UqloBbMi2esllw-sMvGIScoAAoMeAALIWmlLjLdlRXzNC_ArBA', 
+        "Remedial English": 'BQACAgIAAxkBAAMJY2z46Zm4OPHV2TLGVsYkEk2ZfDUAAnweAALIWmlLhNjbnVT6-pkrBA', }
