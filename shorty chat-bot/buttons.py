7a25ee3dc34d73538144be784019b4f13e7e36cb
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-11-10 18:18:35 +0300

    add commands

--- shorty chat-bot/buttons.py
+++ shorty chat-bot/buttons.py
@@ -1,5 +1,15 @@
 from telebot import types
 
+def commands():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton("/start")
+    btn2 = types.KeyboardButton("/commands")
+    btn3 = types.KeyboardButton("/books")
+    btn4 = types.KeyboardButton("/tasks")
+    btn5 = types.KeyboardButton("/homework")
+    btn6 = types.KeyboardButton("/info")
+    return markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+
 
 def get_book():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
@@ -9,3 +19,4 @@ def get_book():
     btn4 = types.KeyboardButton("Перевод англоязычных текстов")
     btn5 = types.KeyboardButton("Remedial English")
     return markup.add(btn1, btn2, btn3, btn4, btn5)
+
